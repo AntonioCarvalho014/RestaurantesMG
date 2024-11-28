fdb666c865c3166daad11447afd03d7d9fa82de1
AntonioCarvalho014 <153518420+AntonioCarvalho014@users.noreply.github.com>
2024-11-28 12:23:46 -0300

    Add files via upload

---
+++ js/restaurantessys.js
@@ -0,0 +1,231 @@
+
+alert('As notas ainda não foram inseridas corretamente.')
+
+
+var notasBurgerSol = [
+ 3.5,
+ 2.5,
+ 3.5,
+ 4.0,
+ 4.5,
+ 4.0,
+ 5.0,
+ 4.0
+]
+
+var soma = 0
+
+for(var i = 0; i < notasBurgerSol.length; i++){
+    var notasBurgers = notasBurgerSol[i]
+
+    soma = soma + notasBurgers
+}
+
+var mediaBurgersol = soma / notasBurgerSol.length
+
+
+
+
+
+
+
+
+var notaspizzariafabiaNa = [
+    4.0,
+    4.0,
+    4.0,
+    1.0,
+    4.0
+    
+    
+   ]
+   
+var soma = 0
+   
+for(var i = 0; i < notaspizzariafabiaNa.length; i++){
+       var notaspizzaff = notaspizzariafabiaNa[i]
+   
+       soma = soma + notaspizzaff
+   }
+   
+var mediafaiana = soma / notaspizzariafabiaNa.length
+
+
+
+
+
+var notasMegalanches = [
+    4.5,
+    2.0,
+    0.5,
+    2.0,
+    4.5,
+    3.0
+    
+    
+   ]
+   
+var soma = 0
+   
+for(var i = 0; i < notasMegalanches.length; i++){
+       var notasmegal = notasMegalanches[i]
+   
+       soma = soma + notasmegal
+   }
+   
+var mediamega = soma / notasMegalanches.length
+
+
+var notasbigssl = [
+   3,
+   4,
+   4
+    
+   ]
+   
+var soma = 0
+   
+for(var i = 0; i < notasbigssl.length; i++){
+       var notabbsl = notasbigssl[i]
+   
+       soma = soma + notabbsl
+   }
+   
+var mediabigs = soma / notasbigssl.length
+
+
+
+var notatiadina = [
+   2.0,
+   2.0,
+   3.5
+
+     
+    ]
+    
+ var soma = 0
+    
+ for(var i = 0; i < notatiadina.length; i++){
+        var notatd = notatiadina[i]
+    
+        soma = soma + notatd
+    }
+    
+ var mediatd = soma /notatiadina.length
+ 
+ 
+
+
+
+var padariasl = [
+   3.5,
+   5.0,
+   4.0,
+   2.5,
+   3.5,
+   4.5,
+
+     
+    ]
+    
+ var soma = 0
+    
+ for(var i = 0; i < padariasl.length; i++){
+        var psl =padariasl[i]
+    
+        soma = soma + psl
+    }
+    
+ var mediapadaria = soma /padariasl.length
+ 
+ 
+
+ var ciro = [
+   4.0,
+   4.0,
+   4.0,
+   4.0,
+   4.0,
+   4.5
+      
+     ]
+     
+  var soma = 0
+     
+  for(var i = 0; i < ciro.length; i++){
+         var cici =ciro[i]
+     
+         soma = soma + cici
+     }
+     
+  var mediaciro = soma /ciro.length
+  
+  
+ 
+
+  var mamamia = [
+   5.0,
+   4.0,
+   4.0,
+   4.5,
+
+       
+      ]
+      
+   var soma = 0
+      
+   for(var i = 0; i < mamamia.length; i++){
+          var mm =mamamia[i]
+      
+          soma = soma + mm
+      }
+      
+   var mediamm = soma /mamamia.length
+   
+
+  
+var gelo = [
+      4.0,
+      3.0,
+      3.0,
+      3.0,
+      2.5
+   
+          
+         ]
+         
+      var soma = 0
+         
+      for(var i = 0; i <gelo.length; i++){
+             var kg =gelo[i]
+         
+             soma = soma + kg
+         }
+         
+      var mediagelo = soma /gelo.length
+      
+   
+
+var rbom = [
+         4.5,
+         5.0,
+         4.0,
+         4.0,
+         4.0
+
+         
+      
+             
+            ]
+            
+         var soma = 0
+            
+         for(var i = 0; i <rbom.length; i++){
+                var rbm = rbom[i]
+            
+                soma = soma + rbm
+            }
+            
+         var mediarbom = soma /rbom.length
+           
+      
